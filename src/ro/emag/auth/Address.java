61c60f4fc0083a30052fbc3a14a951d35bfdbe39
PlacuIsFake <pastefanel@gmail.com>
2026-03-12 08:58:57 +0200

    Implementare fara AI a clasei Address

--- src/ro/emag/auth/Address.java
+++ src/ro/emag/auth/Address.java
@@ -1,10 +1,10 @@
 package ro.emag.auth;
 
-import java.util.List;
+import java.util.UUID;
 
 public class Address {
-    private int id;
-    private int userId;
+    private UUID id;
+    private UUID userId;
     private String label;
     private String street;
     private String city;
@@ -12,49 +12,92 @@ public class Address {
     private String postalCode;
     private String country;
     private boolean isDefault;
-    private User user;
-    private List<Object> orders;
-    private List<Object> lockers;
 
-    public Address() {}
+    public UUID getId() {
+        return id;
+    }
 
-    public Address(int id, int userId, String street, String city, String county, String postalCode, String country) {
+    public void setId(UUID id) {
         this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
         this.userId = userId;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
         this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
         this.county = county;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
         this.country = country;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public int getUserId() { return userId; }
-    public void setUserId(int userId) { this.userId = userId; }
-    public String getLabel() { return label; }
-    public void setLabel(String label) { this.label = label; }
-    public String getStreet() { return street; }
-    public void setStreet(String street) { this.street = street; }
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
-    public String getCounty() { return county; }
-    public void setCounty(String county) { this.county = county; }
-    public String getPostalCode() { return postalCode; }
-    public void setPostalCode(String postalCode) { this.postalCode = postalCode; }
-    public String getCountry() { return country; }
-    public void setCountry(String country) { this.country = country; }
-    public boolean isDefault() { return isDefault; }
-    public void setDefault(boolean isDefault) { this.isDefault = isDefault; }
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public String getFormatedAddress() {
+        return "";
+    }
 
-    public String getFormattedAddress() { return ""; }
-    public void markAsDefault() {}
-    public void unmarkAsDefault() {}
+    public void markAsDefault() {
+        this.isDefault = true;
+    }
+
+    public void unmarkAsDefault() {
+        this.isDefault = false;
+    }
 
     @Override
     public String toString() {
-        return "Address{id=" + id + ", street=" + street + ", city=" + city + "}";
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", county='" + county + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
     }
 }
